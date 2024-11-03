46b4c4950f165eb83dd1970058de4bbcdd03ce45
EsraGurbuzf <230543001@firat.edu.tr>
2024-11-03 13:11:06 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main(void){
+  int e = 0;
+  int sum = 0;
+  int count = 0;
+  while(e != -1){
+    printf("Write a number: \n");
+    scanf("%d", &e);
+    if(e != -1){
+      sum = sum + e;
+      count++;
+    }
+    if (e == -1) {
+      break;  
+    }
+  } 
+  float average = (float)sum / count;
+  printf("%d numbers typed.\n", count);
+  printf("The sum is: %d\n", sum);
+  printf("The average is: %.1f", average);
+}
