d7f9859cedb691063b4fb339033a66eb098db599
EsraGurbuzf <230543001@firat.edu.tr>
2024-11-03 13:31:14 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(void){
+  int seq[10];
+  int i;
+  int sum = 0;
+  float average = 0;
+  printf("Write 10 numbers:\n");
+  for(i = 0; i < 10; i++){
+    scanf("%d", &seq[i]);
+    sum += seq[i];
+  }
+  average = sum / 10.0;
+  printf("The sum is %d\n", sum);
+  printf("The average is %.1f\n", average);
+}
