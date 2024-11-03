155d61a686e61014fa3673272dc20d826781cf6f
EsraGurbuzf <230543001@firat.edu.tr>
2024-11-03 20:43:18 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(void){
+  int r;
+  int c; 
+  for(r = 1; r <= 5; r++){
+    for(c = 1; c <= 5; c++){
+      if (r == c || r == 6 - c) {
+        printf("*");
+      } else {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
