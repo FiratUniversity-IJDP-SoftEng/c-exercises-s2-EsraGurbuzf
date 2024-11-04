40000ea30bf30981118fe6dcb67b21decf2d6802
EsraGurbuzf <230543001@firat.edu.tr>
2024-11-04 20:17:50 +0300

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+int factorial(int e);
+int main(void){
+  int e;
+  do{
+  printf("Write a number between 0 and 50:\n");
+  scanf("%d",&e);
+  }while(e<0 || e>50);
+  int result = factorial(e);
+  printf("The factorial of %d is %d\n", e, result);
+  return 0;
+}
+int factorial(int e){
+  int result = 1;
+  int i;
+  for(i = 1; i <= e; i++){
+    result *= i;
+  }
+  return result;
+}
