f67b3bc9e5882fdbee43a82ff2f63b21c3c53311
EsraGurbuzf <230543001@firat.edu.tr>
2024-11-04 19:09:04 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main(void) {
+  int i;
+  int e;
+  int result = 1;
+  do {
+    printf("Write a number between 1 and 50\n");
+    scanf("%d", &e);
+  } while (e < 1 || e > 50);
+  for (i = 1; i <= e; i++) {
+    result *= i;
+  }
+  printf("The factorial of %d is %d\n", e, result);
+}
 
